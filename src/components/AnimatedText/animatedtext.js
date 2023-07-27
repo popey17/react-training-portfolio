a87d0919fe168939b688cf74ef805fd6e0b5a64b
Aung Myat Kyaw <aungmyatkyaw@Aungs-MacBook-Pro.local>
2023-07-27 12:30:04 +0630

    home page text animation

---
+++ src/components/AnimatedText/animatedtext.js
@@ -0,0 +1,17 @@
+import './animated.sass';
+
+const AnimatedLetters = ({ letterClass, strArr , idx }) => {
+    return (
+        <span>
+            {
+                strArr.map((char, i) =>(
+                    <span key={char+i} className={`${letterClass} _${i + idx}`}>
+                        {char}
+                    </span>
+                ))
+            }
+        </span>
+    )
+}
+
+export default AnimatedLetters;
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,20 +1,48 @@
-import Logo from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Logo from './Logo/logo'
 import './home.sass'
+import AnimatedLetters from '../AnimatedText/animatedtext'
 
 const Home = () => {
+    const[letterClass, setLetterClass] = useState('text-animate')
+    const nameArr = ['l','o','b','o','d','a','n']
+    const jobArr = ['w','e','b',' ','d','e','v','e','l','o','p','e','r','.']
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        return () => {
+            clearTimeout(timeoutId);
+        };
+    }, []);
+    
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br/> I'm
-                <img src={Logo} alt=""/>
-                lobodan
+                <h1>
+                <span className={letterClass}>H</span>    
+                <span className={`${letterClass} _12`}>i,</span>    
+                <br/>
+                <span className={`${letterClass} _13`}>I</span> 
+                <span className={`${letterClass} _14`}>'m</span> 
+                <img src={LogoTitle} alt=""/>
+                <AnimatedLetters letterClass={letterClass}
+                strArr={nameArr}
+                idx={15} />
                 <br />
-                web developer
+                <AnimatedLetters letterClass={letterClass}
+                strArr={jobArr}
+                idx={22} />
                 </h1>
                 <h2>Full Stack Developer </h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            < Logo />
         </div>
     )
 }
